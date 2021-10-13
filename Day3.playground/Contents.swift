3cedd154f427ed519eb2dbc4c1d24a6142de059f
shrutinikose <nikoseshruti@gmail.com>
2021-10-13 19:18:11 +0530

    Conditionals, Combining conditions, The ternary operator

--- Day3.playground/Contents.swift
+++ Day3.playground/Contents.swift
@@ -42,4 +42,51 @@ print(firstScore != secondScore)
 print("Taylor" <= "Swift")
 
 
+// Conditionals
+var firstCard = 11
+var secondCard = 10
+
+if firstCard + secondCard == 21 {
+    print("Blackjack!")
+}
+if firstCard + secondCard == 21 {
+    print("Blackjack!")
+} else {
+    print("Regular cards")
+}
+if firstCard + secondCard == 2 {
+    print("Aces – lucky!")
+} else if firstCard + secondCard == 21 {
+    print("Blackjack!")
+} else {
+    print("Regular cards")
+}
+
+// Combining conditions
+
+let age1 = 12
+let age2 = 21
+        // and
+if age1 > 18 && age2 > 18 {
+    print("Both are over 18")
+}
+if age1 > 18, age2 > 18 {
+    print("Both are over 18")
+}
+        // or
+if age1 > 18 || age2 > 18 {
+    print("At least one is over 18")
+}
+
+// The ternary operator
+firstCard = 12
+secondCard = 11
+let isTrue = firstCard == secondCard ? "both cards are same" : "cards are not same"
+print(isTrue)
+
+s
+
+
+
+
 
