c5787fb768406d231b7e1835e0e36b8ec9b3b45e
shrutinikose <nikoseshruti@gmail.com>
2021-10-13 19:06:34 +0530

    Operators overloading

--- Day3.playground/Contents.swift
+++ Day3.playground/Contents.swift
@@ -2,6 +2,7 @@ import UIKit
 
 var str = "Hello, playground"
 
+//Arithmetic Operations
 let firstScore = 12
 let secondScore = 4
 let total = firstScore + secondScore
@@ -11,5 +12,18 @@ print("this is total : \(difference)")
 
 let remainder = 13 % secondScore
 print(remainder)
+
+// Operators overloading
+let meaningOfLife = 42
+let doubleMeaning = 42 + meaningOfLife
+
+let fakers = "fakers gonna "
+let action = fakers + "action"
+print(action)
+
+let firstHalf = ["John", "Paul"]
+let secondHalf = ["George", "Ringo"]
+let beatles = firstHalf + secondHalf
+print(beatles)
 
 
