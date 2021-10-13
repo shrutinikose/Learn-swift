ca363a29718a68013a3ce532670c824fe035cc13
shrutinikose <nikoseshruti@gmail.com>
2021-10-13 19:02:42 +0530

    Arithmetic operations

---
+++ Day3.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let firstScore = 12
+let secondScore = 4
+let total = firstScore + secondScore
+let difference = firstScore - secondScore
+print("this is total : \(total)")
+print("this is total : \(difference)")
+
+let remainder = 13 % secondScore
+print(remainder)
+
+
